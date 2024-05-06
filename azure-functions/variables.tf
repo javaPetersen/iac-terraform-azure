8f741f4ef5b471aed3291b3afc10c307b1a593dd
javaPetersen <perlinskipiotr@wp.pl>
2024-05-06 17:13:06 +0200

    fixes

--- azure-functions/variables.tf
+++ azure-functions/variables.tf
@@ -1,91 +1,93 @@
 variable "resource_group_name" {
-	type = string
-	default = "Example"
+  type    = string
+  default = "Example"
 }
 variable "resource_group_location" {
-	type = string
-	default = "West Europe"
+  type    = string
+  default = "West Europe"
 }
 
 variable "storage_account_name" {
-	type = string
-	default = "storageaccountnamewsbpp"
+  type    = string
+  default = "storageaccountnamewsbpp"
 }
 
 variable "storage_account_account_tier" {
-	type = string
-	default = "Standard"
+  type    = string
+  default = "Standard"
 }
 
 variable "storage_account_account_replication_type" {
-	type = string
-	default = "GRS"
+  type    = string
+  default = "GRS"
 }
 
 variable "storage_account_tags_env" {
-	type = string
-	default = "staging"
+  type    = string
+  default = "staging"
 }
 
 variable "service_plan_name" {
-	type = string
-	default = "service_name"
+  type    = string
+  default = "service_name"
 }
 
 variable "service_plan_os_type" {
-	type = string
-	default = "Linux"
+  type    = string
+  default = "Linux"
 }
 
 variable "service_plan_sku_name" {
-	type = string
-	default = "P1v2"
+  type    = string
+  default = "P1v2"
 }
 
 variable "linux_function_app_name" {
-	type = string
-	default = "linux-function-app"
+  type    = string
+  default = "linux-function-app"
 }
 
 variable "app_function_name" {
-	type = string
-	default = "example-function-app-function"
+  type    = string
+  default = "example-function-app-function"
 }
 
 variable "app_function_config_json" {
-	type = string
-	default = <<EOF
-	
-    "bindings" = [
-      {
-        "authLevel" = "function"
-        "direction" = "in"
-        "methods" = [
+  type = string
+  default = <<EOF
+{
+  "bindings": [
+    {
+        "authLevel": "function",
+        "direction": "in",
+        "methods": [
           "get",
-          "post",
-        ]
-        "name" = "req"
-        "type" = "httpTrigger"
-      },
-      {
-        "direction" = "out"
-        "name"      = "$return"
-        "type"      = "http"
-      },
-    ]
-  EOF
+          "post"
+        ],
+        "name": "req",
+        "type": "httpTrigger"
+    },
+    {
+        "direction": "out",
+        "name": "$return",
+        "type": "http"
+    }
+  ]
+}
+EOF
 }
 
 variable "app_function_test_data" {
-	type = string
-	default = <<EOF
-    "name" = "Azure"
- EOF
+  default = <<EOF
+{
+  "name": "Azure"
+}
+EOF
 }
 
 variable "app_function_language" {
-	type = string
-	default = "Python"
+  type    = string
+  default = "Python"
 }
 
 
